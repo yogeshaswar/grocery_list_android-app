0eda62e743d4e782dce67a5d60e23bedd7345fdd
Yogesh Aswar <yogeshaswar46@gmail.com>
2023-06-04 15:46:10 +0530

    feature - alert box on back pressed

--- app/src/main/java/com/yogeshaswar/grocerylist/MainActivity.java
+++ app/src/main/java/com/yogeshaswar/grocerylist/MainActivity.java
@@ -167,16 +167,38 @@ private List<GroceryItem> groceryList = new ArrayList<>();
         return userDecision;
    }
 
-//    @Override
-//    public void onBackPressed() {
-//
-//        super.onBackPressed();
-//        AtomicBoolean decision = showAlert("Alert", "Do you want to close the app?", "No", "Yes");
-//        if(decision.equals(true)) {
-//            return;
-//        } else {
-//            Toast.makeText(this, "closing", Toast.LENGTH_SHORT).show();
-//        }
-//        finish();
-//    }
-}
+    // Declare the onBackPressed method when the back button is pressed this method will call
+    @Override
+    public void onBackPressed() {
+        // Create the object of AlertDialog Builder class
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+        // Set the message show for the Alert time
+        builder.setMessage("Do you want to exit ?");
+
+        // Set Alert Title
+        builder.setTitle("Alert !");
+
+        // Set Cancelable false for when the user clicks on the outside the Dialog Box then it will remain show
+        builder.setCancelable(false);
+
+        // Set the positive button with yes name Lambda OnClickListener method is use of DialogInterface interface.
+        builder.setPositiveButton("Yes", (DialogInterface.OnClickListener) (dialog, which) -> {
+            // When the user click yes button then app will close
+            finish();
+        });
+
+        // Set the Negative button with No name Lambda OnClickListener method is use of DialogInterface interface.
+        builder.setNegativeButton("No", (DialogInterface.OnClickListener) (dialog, which) -> {
+            // If user click no then dialog box is canceled.
+            dialog.cancel();
+        });
+
+        // Create the Alert dialog
+        AlertDialog alertDialog = builder.create();
+        // Show the Alert Dialog box
+        alertDialog.show();
+
+
+
+    }}
